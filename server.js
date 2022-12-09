1027b88568545bdc04ca3db8c165653ae81ab302
trnthanhha <115056099+trnthanhha@users.noreply.github.com>
2022-12-10 06:40:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 var os = require("os");
-
+const PORT = process.env.PORT || 3000
 app.get("/", function (req, res) {
   res.json({
     serverName: "hiamt34-server",
     ip: "123.17.159.125",
-    port: process.env.PORT,
+    port: PORT,
     domain: "hiamt9a.online",
     memory: os.totalmem(),
     os: os.platform(),
@@ -17,6 +17,6 @@ app.get("/", function (req, res) {
   });
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log("run server success!!");
 });
